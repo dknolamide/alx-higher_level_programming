ecc8f8dc07da23aef227ad85e828871106a94c06
Olamide <dknolamide@gmail.com>
2022-08-23 05:55:24 -0700

    101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+table File  9 lines (9 sloc)  158 Bytes
+
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new = ""
+    i = 0
+    for c in str:
+        if i != n:
+            new += c
+        i += 1
+    return new
